ca2c4dd7570cce9c5da26ecf18b06f7180e1d536
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2023-04-22 14:26:59 +0530

    Added test case for alphabet case

--- tests/rules/alphanumSpec.js
+++ tests/rules/alphanumSpec.js
@@ -6,11 +6,17 @@ import rules from "../../src/rules";
 
 describe("alphanum", () => {
   const validAlphanumeric = "AAaaa2101202";
+  const validUppercaseAlphabets = "ABBCCCDDDD";
   const invalidAlphanumeric = "AAAa   ss224";
   const input = document.createElement("input");
 
   it("should return true on alphanumeric input", () => {
     input.value = validAlphanumeric;
+    expect(rules.alphanum(input)).toBeTruthy();
+  });
+  
+  it("should return true on alphanumeric input", () => {
+    input.value = validUppercaseAlphabets;
     expect(rules.alphanum(input)).toBeTruthy();
   });
 
